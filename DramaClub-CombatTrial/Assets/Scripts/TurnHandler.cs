2e3baded2bd87dcfcdf6795048653a19af76d82d
LiamMackenzie <49274951+LiamMackenzie@users.noreply.github.com>
2019-05-02 19:25:59 +0800

    The Game..... like all of it

---
+++ DramaClub-CombatTrial/Assets/Scripts/TurnHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TurnHandler 
+{
+    public string Attacker;//name of the atacker
+    public GameObject AttackersGameObject;//who is attacking
+    public GameObject AttackersTarget;//whos going to be attacked
+
+    //which attack is preformed
+}

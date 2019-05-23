285d4e2a835097cd943f345fee260a4c0f6c99d1
LiamMackenzie <liam_mackenzie56@hotmail.com>
2019-05-23 19:14:42 +0800

    Button_build

---
+++ CombatTrail/DramaClub-CombatTrial/Assets/Scripts/NewScripts/Dummy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+   public void dummy1()
+   {
+       print ("Hey i am pressed");
+   }
+}

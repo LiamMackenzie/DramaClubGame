392129b75e35197ef175e36dcebd766b01405846
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-08-06 10:24:03 +0800

    Newgame

---
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/CameraScripts/SwitchCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCharacter : MonoBehaviour
+{
+    public Transform partySpawn;
+    public GameObject character1;
+    public GameObject character2;
+    public GameObject characterInPlay;
+    public GameObject camera1;
+    public GameObject camera2;
+    public GameObject camera3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+			{
+				Switch1();
+			}
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Switch2();
+        }
+    }
+
+    void Switch1()
+    {
+        character1.SetActive(true);
+        camera1.SetActive(true);
+        camera3.SetActive(false);
+        characterInPlay.SetActive(false);
+
+    }
+
+    void Switch2()
+    {
+          character2.SetActive(true);
+        camera2.SetActive(true);
+        camera3.SetActive(false);
+        characterInPlay.SetActive(false);
+    }
+}
---
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float speed;
+	public float jumpHeight;
+	private float movement;
+	
+	private bool faceright = true;
+	private Rigidbody2D rb;
+	
+	private bool onground;
+	public Transform floorCheck;
+	public float checkRadius;
+	public LayerMask whatGround;
+
+	private int moreJump;
+	public int moreJumpVal;
+
+	// Use this for initialization
+	void Start () {
+		moreJump = moreJumpVal;
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		onground = Physics2D.OverlapCircle(floorCheck.position, checkRadius, whatGround);
+
+		movement = Input.GetAxis("Horizontal");
+		rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+	
+	}
+	void Update()
+	{
+		if(faceright == true)
+		{
+			if (Input.GetKeyDown(KeyCode.A))
+				{
+					transform.Rotate(0f, 180f, 0f);
+					faceright = false;
+				}
+		}
+		else if(faceright == false)
+			{
+				if(Input.GetKeyDown(KeyCode.A))
+					{
+						transform.Rotate(0f, 0f, 0f);
+					}
+			}
+
+		if(faceright == false)
+		{
+			if (Input.GetKeyDown(KeyCode.D))
+				{
+					transform.Rotate(0f, 180f, 0f);
+					faceright = true;
+				}
+				
+		}
+		
+
+		if(onground == true)
+		{
+			moreJump = moreJumpVal;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space) && moreJump > 0)
+		{
+			rb.velocity = Vector2.up * jumpHeight;
+			moreJump--;
+		}
+		else if(Input.GetKeyDown(KeyCode.Space) && moreJump == 0 && onground == true)
+		{
+			rb.velocity = Vector2.up * jumpHeight;
+		}
+		
+	
+		
+	}
+	
+
+	
+	
+}

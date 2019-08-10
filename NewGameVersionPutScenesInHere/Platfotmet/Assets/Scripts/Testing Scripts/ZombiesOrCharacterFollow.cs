0ae365bc812765dd41a1709100b57f79216416dd
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-08-10 16:56:20 +0800

    Waypoints for zombie type characters

---
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/Testing Scripts/ZombiesOrCharacterFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiesOrCharacterFollow : MonoBehaviour
+{
+    private GameObject wP;
+    private Vector3 wayPointPos;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wP = GameObject.Find("WayPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wayPointPos = new Vector3(wP.transform.position.x,
+         transform.position.y, wP.transform.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, 
+        wayPointPos, speed * Time.deltaTime);
+    }
+}

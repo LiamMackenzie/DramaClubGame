0457601f68eddcd4b3a12311fdf765281a86ef51
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-08-12 21:26:38 +0800

    Amanda Anination and Character Updates

---
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/PlayerAnimations/AmandaAnimationCode/AmandaAni.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmandaAni : MonoBehaviour
+{
+     Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+				{
+					animator.Play("Base Layer.Amanda Run");
+				}
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.Play ("Base Layer.Amanda");
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            animator.Play("Base Layer.Amanda Run");
+        }
+
+          if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.Play ("Base Layer.Amanda");
+        }
+
+    }
+}

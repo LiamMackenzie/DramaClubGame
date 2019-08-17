56cd5ade4a713dbd30e9377bd908fcaedc84cc70
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-08-17 14:14:53 +0800

    AHHHHHHHHH

--- NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/Weapon.cs
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/Weapon.cs
@@ -10,7 +10,7 @@ public class Weapon : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetKeyDown(KeyCode.LeftControl))
+		if (Input.GetKeyDown(KeyCode.J))
 			{
 				Shoot();
 			}

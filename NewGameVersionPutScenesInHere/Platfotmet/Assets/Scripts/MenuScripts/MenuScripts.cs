7c3bdef9a3673bb80f6f43629aa1c74df7069f6f
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-09-17 09:58:31 +0800

    JasonPrefabsUpdate

---
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/MenuScripts/MenuScripts.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour
+{
+
+    public static bool GamePause = false;
+
+    public GameObject pauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void hubLoad()
+    {
+        SceneManager.LoadScene("MainHub");
+    }
+
+    public void quitting()
+    {
+        Debug.Log("quitting game sir");
+        Application.Quit();
+    }
+
+    public void resuming()
+    {
+        pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GamePause = false;
+    }
+    
+}

89651a61626796df7fff59bbf6ba0aef7db4c5b6
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-09-19 16:16:40 +0800

    Lava Stage Update

---
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/timers/MovingPlatformVertical.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformVertical : MonoBehaviour
+{
+    public float timer;
+    public float timerReset;
+    private Rigidbody2D rb;
+    public bool moving;
+    public float verticalHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+      
+        moving = true;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         timer -= Time.deltaTime;
+    
+
+         if(timer <= 0)
+         {
+             if(moving == true)
+             {
+                   rb.velocity = -Vector2.up * verticalHeight;
+                   moving = false;
+                   timer = timerReset;
+             }
+             
+             else if(moving == false)
+             {
+                  rb.velocity = Vector2.up * verticalHeight;
+                   moving = true;
+                   timer = timerReset;
+             }
+         }
+         
+    
+
+    
+    }
+}

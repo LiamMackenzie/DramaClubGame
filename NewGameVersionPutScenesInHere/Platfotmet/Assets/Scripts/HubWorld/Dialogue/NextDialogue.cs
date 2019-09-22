b22a10c733dde67326e0b5a9befd6f4a41040822
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-09-22 19:02:21 +0800

    HubWorld Dialogue Update

---
+++ NewGameVersionPutScenesInHere/Platfotmet/Assets/Scripts/HubWorld/Dialogue/NextDialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextDialogue : MonoBehaviour
+{
+    
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject Deactivate;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            text1.SetActive(false);
+            text2.SetActive(true);
+            Deactivate.SetActive(false);
+        }
+
+         if(Input.GetKeyDown(KeyCode.Space))
+        {
+            text1.SetActive(false);
+            text2.SetActive(true);
+            Deactivate.SetActive(false);
+        }
+    }
+}

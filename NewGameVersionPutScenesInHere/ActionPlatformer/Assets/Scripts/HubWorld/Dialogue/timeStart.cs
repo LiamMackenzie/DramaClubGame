454b2c0d84ca9b39a64b9f8d0b299a77ceb1dfa6
Jason Flores <49274929+AngryLoveBite@users.noreply.github.com>
2019-09-23 23:04:07 +0800

    DialogueGrrGrr

---
+++ NewGameVersionPutScenesInHere/ActionPlatformer/Assets/Scripts/HubWorld/Dialogue/timeStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+           Time.timeScale = 1f;
+        }
+         if(Input.GetKeyDown(KeyCode.Return))
+        {
+           Time.timeScale = 1f;
+        }
+    }
+}

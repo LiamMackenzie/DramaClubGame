e0e3b478f3b75de4dc5dd56fd8067029d0b840e3
Dylan Davies <FreshlySketchedGames@gmail.com>
2019-10-07 19:48:07 +0800

    Update GrrGrr_DD.cs

--- NewGameVersionPutScenesInHere/ActionPlatformer/Assets/Scripts/GrrGrr_DD.cs
+++ NewGameVersionPutScenesInHere/ActionPlatformer/Assets/Scripts/GrrGrr_DD.cs
@@ -4,127 +4,66 @@ using UnityEngine;
 
 public class GrrGrr_DD : MonoBehaviour
 {
-float dirX;
 
-	[SerializeField]
-	float moveSpeed = 3f;
+public float gr_MoveSpeed;
+public float gr_AttackingDistance;
 
-    [SerializeField]
-	float smallJumpForce = 300f;
+private float nextAttack;
+public float attackRate = 1.0f;
 
-    [SerializeField]
-	float bigJumpForce = 500f;
+private Transform gr_Target;
+private Animator anim;
+private SpriteRenderer gr_SpriteRenderer;
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
+void Start() 
+{
+	gr_Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	gr_SpriteRenderer = GetComponent<SpriteRenderer>();
+	gr_MoveSpeed = 3f;
+	gr_AttackingDistance = 10f;
 
-	Rigidbody2D rb;
+}
 
-	bool facingRight = false;
+void Update() 
+{
+	MoveAndAttack();
+}
 
-	//Vector3 localScale;
-	private float movement;
+void LateUpdate() 
+{
+	FaceDirection();
+}
 
-    //public Transform target;
-	public GameObject target;
-	//private int targetIndex = 0;
-	private Vector2 targetPos;
-
-
-    //public float stoppingDistance = 10f;
-
-	// Use this for initialization
-	void Start () {
-		//localScale = transform.localScale;
-		rb = GetComponent<Rigidbody2D> ();
-		//dirX = -1f;
-
-        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-		target = GameObject.FindGameObjectWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        //targetChange();
-	}
-
-	void FixedUpdate()
+void FaceDirection() //Assuming player is on left of grrgrr at start
+{
+	if (transform.position.x - gr_Target.position.x < 0f)
 	{
-        //rb.velocity = new Vector2 (dirX * moveSpeed, rb.velocity.y);
-		targetPos = new Vector2(target.transform.position.x,
-         transform.position.y);
-		 CheckWhereToFace(movement);
-
-        transform.position = Vector2.MoveTowards(transform.position, 
-        targetPos, moveSpeed * Time.deltaTime);
-
+		gr_SpriteRenderer.flipX = true;
 	}
-
-	void LateUpdate()
+	else
 	{
-		//CheckWhereToFace ();
+		gr_SpriteRenderer.flipX = false;
 	}
+}
 
-	void CheckWhereToFace(float movement)
+void MoveAndAttack()
+{
+	if(Vector2.Distance(transform.position, gr_Target.position) > gr_AttackingDistance)
 	{
-		//if (dirX > 0)
-			//facingRight = true;
-		//else if (dirX < 0)
-			//facingRight = false;
+		transform.position = Vector2.MoveTowards(transform.position, gr_Target.position, gr_MoveSpeed * Time.deltaTime);
+	}
+	if(Vector2.Distance(transform.position, gr_Target.position) < gr_AttackingDistance)
+	{
+		if(Time.time > nextAttack)
+        {
+            nextAttack = Time.time + attackRate;
+
+			anim.SetBool("isAttacking", true);
 			
-
-	//	if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
-			//localScale.x *= -1;
-
-		//transform.localScale = localScale;
-
-		if(movement > 0 && !facingRight || movement < 0 && facingRight)
-		{
-			facingRight = !facingRight;
-
-			Vector2 theScale = transform.localScale;
-
-			theScale.x *= -1;
-
-			transform.localScale = theScale;
+			//Instantiate(gr_Projectile
 		}
+		//Debug.Log("Attacking");
 	}
-
-	void OnTriggerEnter2D (Collider2D col)
-	{
-		switch (col.tag) {
-
-		case "GrrJump":
-			rb.AddForce (Vector2.up * smallJumpForce);
-			break;
-
-        case "GrrJumpBig":
-			rb.AddForce (Vector2.up * bigJumpForce);
-			break;
-
-		case "WallLeft":
-			dirX = 1f;
-			break;
-
-		case "WallRight":
-			dirX = -1f;
-			break;
-		}	
-	}
-		//void ChangeTargets()
-	//	{
-		//	targetIndex += 1;
-		//	if (targetIndex >= potentialTargets.Length)
-		//	{
-		//		targetIndex = 0;
-		//	}
-		//	target = potentialTargets[targetIndex];
-	//	}
-//
-	//	   	void targetChange()
-	//	{
-		//	if(Input.GetKeyDown(KeyCode.Q))
-	//	{
-	//			ChangeTargets();
-	//		}	
-	//	}
+}
 
 }
